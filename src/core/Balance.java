bfdfa47cedb7cb73740d8f4e861e3fd0bb26cca1
lorenachedid <lpperez06@gmail.com>
2023-05-11 14:22:36 -0400

    Created Balance Page

---
+++ src/core/Balance.java
@@ -0,0 +1,14 @@
+package core;
+
+public class Balance {
+    public static void main(String[] args) {
+
+        // practicing ideas
+        // practicing with commits
+
+        // this is your balance
+
+
+
+    }
+}
